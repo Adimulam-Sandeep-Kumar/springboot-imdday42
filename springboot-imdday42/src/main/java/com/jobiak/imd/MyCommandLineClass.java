b889b2c5e62ede3e2e59fa73b4887f0bf67c78f8
Sandeep Kumar <sandeepkumar.adimulam@jobiak.com>
2022-05-17 10:15:06 +0530

    project architect initiating the first push of project src

---
+++ springboot-imdday42/src/main/java/com/jobiak/imd/MyCommandLineClass.java
@@ -0,0 +1,52 @@
+package com.jobiak.imd;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.jobiak.imd.model.Customer;
+import com.jobiak.imd.repository.CustomerRepo;
+
+@Component
+public class MyCommandLineClass implements CommandLineRunner{
+
+	@Autowired
+	CustomerRepo repo;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Customer c1 = new Customer();
+		c1.setAcctno(101);c1.setName("Xiang");c1.setBalance(4000);
+		repo.save(c1);
+		
+		Customer c2 = new Customer();
+		c2.setAcctno(102);c2.setName("Nobitha");c2.setBalance(5000);
+		repo.save(c2);
+		
+		Customer c3 = new Customer();
+		c3.setAcctno(103);c3.setName("Deki-suki");c3.setBalance(9000);
+		repo.save(c3);
+		
+		Customer c4 = new Customer();
+		c4.setAcctno(104);c4.setName("Sichan");c4.setBalance(7000);
+		repo.save(c4);
+		
+		
+		Optional<Customer> cust = repo.findById(102L);
+		if(cust.isPresent()) {
+			System.out.println(cust.get());
+		}
+		
+		List<Customer> customers = repo.findAll();
+		
+		for(Customer c : customers) {			
+			System.out.println(c);
+		}
+		
+	}
+
+}
